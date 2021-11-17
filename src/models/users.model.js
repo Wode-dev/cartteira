48cca749367876c704f88025b6d73093fe331457
alberttocastro <albertto.programmer@gmail.com>
2021-11-17 15:40:33 -0300

    Remove white space

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -27,21 +27,15 @@ module.exports = function (app) {
   const modelName = 'users';
   const mongooseClient = app.get('mongooseClient');
   const schema = new mongooseClient.Schema({
-
     email: { type: String, unique: true, lowercase: true },
     password: { type: String },
-
-
     auth0Id: { type: String },
-
     username: String,
-    password: String,
     fullName: String,
     roles: {
       type: String,
       enum: ['sysadmin', 'admin'],
     },
-
   }, {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
   });
