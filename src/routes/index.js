fa982f2e39e683382fc3a2bd514d5ffff01ba051
alberttocastro <albertto.programmer@gmail.com>
2021-11-21 16:55:52 -0300

    Removes API from URL

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const middlewares = require("../middlewares");
  *                                     ROUTES
  * =============================================================================**/
 const apiV1Routes = require("./v1");
-routes.use("/api/v1", apiV1Routes);
+routes.use("/v1", apiV1Routes);
 
 const apiAuth = require("./auth");
 routes.use("/api/auth", apiAuth);
